c606d4481defb34622a3b7d8d3f74b7987ffcaba
Francisco Mas <framas@MBPdeFrancisco2.home>
2022-02-04 11:39:00 +0100

    programa4

--- programa4.py
+++ programa4.py
@@ -10,6 +10,7 @@ from multiprocessing import Process
 from multiprocessing import current_process
 from multiprocessing import Value
 import time
+
 def f(c):
     for i in range(100):
         temp = c.value + 1
@@ -23,6 +24,8 @@ if __name__ == "__main__":
     c = Value('i', 0)
     for i in range(N):
         lp.append(Process(target=f, args=(c,)))
+    
     print ("Valor inicial del contador", c.value)
     for p in lp:
         p.start()
+        
